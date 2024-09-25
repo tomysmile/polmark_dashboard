985720bb7bd6d64a7a55949b000fddda18b6fb0b
Tomy Ismail <tomysmile@gmail.com>
2024-09-25 03:35:20 +0000

    update fixtures

--- polmark_dashboard/hooks.py
+++ polmark_dashboard/hooks.py
@@ -40,7 +40,9 @@ fixtures = [
     {"doctype": "Organisation Structure Position", "filters": [["standard", "=", 1]]},
     {"doctype": "Zona Pemenangan", "filters": [["standard", "=", 1]]},
     {"doctype": "Peta Zona Pemenangan", "filters": [["standard", "=", 1]]},
-    {"doctype": "Peta Zona Pemenangan Kab Bekasi", "filters": [["standard", "=", 1]]}
+    {"doctype": "Peta Zona Pemenangan Kab Bekasi", "filters": [["standard", "=", 1]]},
+    {"doctype": "Peta Zona Pemenangan Kota Medan", "filters": [["standard", "=", 1]]},
+    {"doctype": "Peta Zona Pemenangan Prov Sumut", "filters": [["standard", "=", 1]]}
 ]
 
 
